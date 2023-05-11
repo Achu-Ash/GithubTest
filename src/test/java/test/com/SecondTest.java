e36416c5f5da0775ddcfc2f20039b83e02e34d7a
Achu-Ash <ashoksmart257964@gmail.com>
2023-05-11 11:34:58 +0530

    new changes on int value

--- src/test/java/test/com/SecondTest.java
+++ src/test/java/test/com/SecondTest.java
@@ -2,14 +2,12 @@ package test.com;
 
 public class SecondTest {
  public static void main(String[] args) {
-	 int a = 10;
+	 int a = 11;
 	  
 	 if (a%2==0) {
 		 System.out.println(a+" is even number ");
-		
 	} else {
 		 System.out.println(a+" is odd number ");
-
 	}
 }
 }
